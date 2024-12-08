3a813946243eec90094c2d4b12c23e22bdd95426
Oliver Grein <ogrein1@t-online.de>
2024-12-08 21:21:13 +0100

    why path

--- crossmodal_training.py
+++ crossmodal_training.py
@@ -106,7 +106,7 @@ class MultiModalFeatureExtractor(nn.Module):
         frames_reshaped = frames.view(batch_size * num_frames, channels, height, width)  # Shape: (batch_size * num_frames, 3, 112, 112)
         
         pil_images = [Image.fromarray(frame.permute(1, 2, 0).byte().cpu().numpy()) for frame in frames_reshaped]
-        aligned_pil_images = [align.get_aligned_face(rgb_pil_image=img) for img in pil_images]
+        aligned_pil_images = [align.get_aligned_face("",rgb_pil_image=img) for img in pil_images]
         aligned_frames = torch.stack([to_input(img) for img in aligned_pil_images])  # Convert and stack tensors
         aligned_frames = aligned_frames.view(batch_size, num_frames, *aligned_frames.shape[1:])  # Reshape back
 
