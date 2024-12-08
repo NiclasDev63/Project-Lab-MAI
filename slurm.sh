e3b54fab8843eabbcf12fc3a118af7201e5f12f1
Oliver Grein <ogrein1@t-online.de>
2024-12-08 21:52:49 +0100

    added slurm file

---
+++ slurm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -e /work/scratch/kurse/kurs00079/data/vox2/out/test.err
+#SBATCH -o /work/scratch/kurse/kurs00079/data/vox2/out/test.out
+#
+# CPU specification
+#SBATCH -n 1 # 1 process
+#SBATCH -c 8 # 8 CPU cores per process
+#SBATCH --mem-per-cpu=1750 # Hauptspeicher in MByte pro Rechenkern
+#SBATCH -t 21:30:00 # in hours:minutes, or '#SBATCH -t 10' - just minutes
+#SBATCH -A kurs00079
+#SBATCH -p kurs00079
+#SBATCH --reservation=kurs00079
+# GPU specification
+#SBATCH --gres=gpu:a100:1 # 1 GPU
+
+module load gcc/8 python/3.9
+cd /work/scratch/kurse/kurs00079/data/vox2/
+source shared_env/shared_env3.9/bin/activate
+cd shared_code/Project-Lab-MAI
+python train_crossmodal
+
+exit $EXITCODE
