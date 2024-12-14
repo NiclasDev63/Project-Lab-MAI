9b95d814755ef866ca4cb5c95a8d4e75ed9e5477
Oliver Grein <ogrein1@t-online.de>
2024-12-14 18:25:07 +0100

    testing

--- model_loader.py
+++ model_loader.py
@@ -3,4 +3,4 @@ import torch
     
 for i in range(1,10):
     checkpoint = torch.load(f'checkpoint_epoch_{i}.pth')
-    print(f"loaded checkpoint {i} with loss {checkpoint["train_loss"]}")
+    print(f"loaded checkpoint {i} with loss {checkpoint['train_loss']}")
